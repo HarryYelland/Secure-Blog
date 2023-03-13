6af3329cefdfb4a15c276d4ad394326ad95db483
Harry <harryyelland@gmail.com>
2023-03-13 15:40:02 +0000

    Added Basic SQL file draft

--- dss.sql
+++ dss.sql
@@ -1,25 +1,25 @@
-DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Posts;
 
-CREATE TABLE User(  
-    User_ID INT AUTO_INCREMENT,  
-    Username VARCHAR(20),  
+CREATE TABLE Users(
+    User_ID SERIAL,
+    Username VARCHAR(20),
     Password VARCHAR(256),
     Email VARCHAR(30),
     Session VARCHAR(256),
-    2FA INTEGER,
+    Two_FA INTEGER,
     PRIMARY KEY(User_ID)
-);  
+);
 
 CREATE TABLE Posts(
-    Post_ID serial AUTO_INCREMENT PRIMARY KEY,
+    Post_ID SERIAL,
     Post_Title VARCHAR(20),
     Post_Body VARCHAR(200),
     Author INT,
     Is_Private BOOLEAN,
     PRIMARY KEY(Post_ID),
-    CONSTRAINT User
+    CONSTRAINT Users
       FOREIGN KEY(Author)
-        REFERENCES User(User_ID)
+        REFERENCES Users(User_ID)
 	ON DELETE CASCADE
 );
