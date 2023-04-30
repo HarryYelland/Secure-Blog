05deeb96c025771b0e07a5eefa2737b6aba5bcaf
Harry Yelland <mec19tqu@uea.ac.uk>
2023-04-30 16:34:34 +0100

    Added a page for 2FA

---
+++ frontend/src/components/TwoFactorCheck.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
+// Function to handle submitting item to sales order
+const submit = () => {
+  
+};
+
+// Main function for adding a product to a sales order page
+function TwoFactorCheck() {  
+  return (
+    <div>
+      <h2>Enter 2 FA Code</h2>
+      <p>A 2 Factor Authentication code has been emailed to you, please enter this code below:</p>
+      <form>
+        <label for="2fa">Code</label><br/>
+        <input type="text" id="2fa"/><br/>
+        <br/>
+        <button type="submit" id="userSubmit" onClick={submit}>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
