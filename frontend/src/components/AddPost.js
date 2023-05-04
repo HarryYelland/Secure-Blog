2fa8f018777d99facf0b574ca6b2e711a12cafcf
Harry Yelland <mec19tqu@uea.ac.uk>
2023-05-04 15:19:58 +0100

    Added background portion of recaptcha. Not being displayed currently

--- frontend/src/components/AddPost.js
+++ frontend/src/components/AddPost.js
@@ -1,16 +1,27 @@
 import { useState, useEffect } from "react";
 import Axios from "axios";
+import { ReCAPTCHA } from "react-google-recaptcha";
+
+var bot = true;
 
 const submit = () => {
-  alert("Submitting post...");
-  Axios.post("http://localhost:3001/add-post", {
-    postTitle: document.getElementById("postTitle").value,
-    postText: document.getElementById("postText").value
-  }).then((response) => {
-    console.log(response);
-    alert("Post submitted!");
-  });
+  if(bot == false){
+    alert("Submitting post...");
+    Axios.post("http://localhost:3001/add-post", {
+      postTitle: document.getElementById("postTitle").value,
+      postText: document.getElementById("postText").value
+    }).then((response) => {
+      console.log(response);
+      alert("Post submitted!");
+    });
+  } else {
+    alert("Please complete recaptcha");
+  }
 };
+
+function verify(){
+  bot = true;
+}
 
 function AddPost() {  
   return (
@@ -22,6 +33,11 @@ function AddPost() {
         <br/>
         <label for="postText" placeholder="Type anything here! (within reason lol)">Post something here!</label><br/>
         <textarea id="postText" placeholder="Type anything here! (within reason lol)" cols="40" rows="10"></textarea>
+        <br/>
+        <ReCAPTCHA 
+          sitekey="6LelxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+          onChange={verify()}
+        />
         <br/>
         <button type="submit" id="postSubmit" onClick={submit}>Submit</button>
     </form>
