90e95edbcc36562bf6cfdb9ca40ac66677016140
Harry Yelland <mec19tqu@uea.ac.uk>
2023-05-05 17:51:30 +0100

    Added buttons to view individual post

--- frontend/src/components/ViewPosts.js
+++ frontend/src/components/ViewPosts.js
@@ -2,6 +2,11 @@
 
 import React, {useState, useEffect} from 'react';
 
+function ViewPost(id){
+  //console.log(id)
+}
+
+
 function ViewPosts() {
   const [posts, setPosts] = useState(false);
   var allPosts = {};
@@ -24,17 +29,20 @@ function ViewPosts() {
             setPost1([
               JSON.stringify(posts[0].title),
               JSON.stringify(posts[0].username),
-              JSON.stringify(posts[0].body)
+              JSON.stringify(posts[0].body),
+              JSON.stringify(posts[0].post_id)
             ]);
           setPost2([
               JSON.stringify(posts[1].title),
               JSON.stringify(posts[1].username),
-              JSON.stringify(posts[1].body)
+              JSON.stringify(posts[1].body),
+              JSON.stringify(posts[0].post_id)
             ]);
            setPost3([
               JSON.stringify(posts[2].title),
               JSON.stringify(posts[2].username),
-              JSON.stringify(posts[2].body)
+              JSON.stringify(posts[2].body),
+              JSON.stringify(posts[0].post_id)
             ]);
           } catch (error) {
             
@@ -54,6 +62,7 @@ function ViewPosts() {
         <h3>{post1[0]}</h3>
         <h4>By {post1[1]}</h4>
         <p>{post1[2]}</p>
+        <a onClick={ViewPost(post1[3])}>View</a>
       </div>
       <br/>
       <br/>
@@ -61,6 +70,7 @@ function ViewPosts() {
         <h3>{post2[0]}</h3>
         <h4>By {post2[1]}</h4>
         <p>{post2[2]}</p>
+        <a onClick={ViewPost(post2[3])}>View</a>
       </div>
       <br/>
       <br/>
@@ -68,6 +78,7 @@ function ViewPosts() {
         <h3>{post3[0]}</h3>
         <h4>By {post3[1]}</h4>
         <p>{post3[2]}</p>
+        <a onClick={ViewPost(post3[3])}>View</a>
       </div>
       <a href="/post">Make a post</a>
     </div>
