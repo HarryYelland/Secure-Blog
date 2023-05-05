efb2918ffa93416758ee27f8113ade1b90ee4164
Harry Yelland <mec19tqu@uea.ac.uk>
2023-05-05 16:27:41 +0100

    Made changes to the way the posts are displayed on the blog, only working one first render. Needs fixing.

--- frontend/src/components/TestPosts.js
+++ frontend/src/components/TestPosts.js
@@ -5,10 +5,8 @@ import React, {useState, useEffect} from 'react';
 function TestPosts() {
   const [posts, setPosts] = useState(false);
   var allPosts = {};
+  const [post1, setPost1] = useState([]);
   useEffect(() => {
-    getPosts();
-  }, []);
-  async function getPosts() {
     fetch('http://localhost:3001/all-posts')
       .then(response => {
         response.json()
@@ -17,15 +15,29 @@ function TestPosts() {
           for (let i = 0; i < data.length; i++) {
             allPosts[i] = {"postid": data[i].post_id, "username": data[i].username, "title": data[i].post_title, "body": data[i].post_body};
           }
+          setPosts(allPosts)
+            setPost1([
+              JSON.stringify(posts[0].title),
+              JSON.stringify(posts[0].username),
+              JSON.stringify(posts[0].body)
+            ]);
         })
       })
-    await console.log(allPosts);
-  }
+    //console.log(text)
+    
+  }, [post1]);
   return (
     <div>
       <h1>The Football Blog</h1>
-      {JSON.stringify(allPosts)}
+      <h2>The Latest Posts</h2>
+      <br/>
+      <h3>{post1[0]}</h3>
+      <h4>By {post1[1]}</h4>
+      <br/>
+      <p>{post1[2]}</p>
     </div>
-  );
+  )
+ 
+   
 }
 export default TestPosts;
